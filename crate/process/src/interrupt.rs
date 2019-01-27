e01491cb4ad96fa49f31455e780a5e014c43e711
chyyuu <yuchen@mail.tsinghua.edu.cn>
2019-01-27 18:29:51 +0800

    clean 'm_mode' related codes

--- crate/process/src/interrupt.rs
+++ crate/process/src/interrupt.rs
@@ -1,21 +1,11 @@
 #[inline(always)]
 pub unsafe fn disable_and_store() -> usize {
-    if env!("M_MODE") == "1" {
-        let mstatus: usize;
-        asm!("csrci mstatus, 1 << 3" : "=r"(mstatus) ::: "volatile");
-        mstatus & (1 << 3)
-    } else {
-        let sstatus: usize;
-        asm!("csrci sstatus, 1 << 1" : "=r"(sstatus) ::: "volatile");
-        sstatus & (1 << 1)
-    }
+    let sstatus: usize;
+    asm!("csrci sstatus, 1 << 1" : "=r"(sstatus) ::: "volatile");
+    sstatus & (1 << 1)
 }
 
 #[inline(always)]
 pub unsafe fn restore(flags: usize) {
-    if env!("M_MODE") == "1" {
-        asm!("csrs mstatus, $0" :: "r"(flags) :: "volatile");
-    } else {
-        asm!("csrs sstatus, $0" :: "r"(flags) :: "volatile");
-    }
+    asm!("csrs sstatus, $0" :: "r"(flags) :: "volatile");
 }
